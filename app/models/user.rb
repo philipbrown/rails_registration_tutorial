3e21b39f1da52ef254a78199d7acdec8c6502466
Philip Brown <phil@ipbrown.com>
2016-02-27 11:01:22 +0000

    Add test to assert the presence of the email property on the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+  validates :email, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,9 @@
-require 'test_helper'
+require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @subject = User.new
+  end
+
+  should validate_presence_of(:email)
 end
